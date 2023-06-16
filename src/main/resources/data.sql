97378372100ab65127167c17e0fc200e43cccca7
Jhoan Sebastián Pérez Rueda <jhoansebas0301@gmail.com>
2023-06-15 22:53:54 -0500

    update README.md to better explain how it works

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,13 @@
 INSERT INTO food_court_microservice.categories (id,
                                             description,
                                             name)
-VALUES (1, 'COMIDA ITALIANA', 'PIZZA')
+VALUES (1, 'COMIDA ITALIANA', 'PIZZA');
+INSERT INTO food_court_microservice.categories (id,
+                                            description,
+                                            name)
+VALUES (2, 'PAPAS CON SALCHICHA', 'SALCHIPAPAS');
+INSERT INTO food_court_microservice.categories (id,
+                                            description,
+                                            name)
+VALUES (3, 'ENSALADA DE TOMATE, CEBOLLA Y LECHUGA', 'ENSALADA MIXTA');
+
